0c3a36c382e93c8138599909fa4af98d8847c2e0
Joey Smith <jsmith@webinertia.net>
2026-09-05 03:03:50 -0500

    chore: remove per-file copyright headers
    File-level 'This file is part of...' copyright docblocks (including 42 stale 'Webware Farmers Store Inventory' headers) removed; the root LICENSE covers attribution.

--- src/Middleware/IdentityMiddleware.php
+++ src/Middleware/IdentityMiddleware.php
@@ -2,16 +2,6 @@
 
 declare(strict_types=1);
 
-/**
- * This file is part of the Webware\UserManager package.
- *
- * Copyright (c) 2026 Joey Smith <jsmith@webinertia.net>
- * and contributors.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Webware\UserManager\Middleware;
 
 use Mezzio\Session\RetrieveSession;
